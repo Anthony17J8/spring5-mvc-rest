2be56de28857223fb1c581cb63bd139db2e1e055
Anton Kiselev <j8kiselev@gmail.com>
2020-01-29 21:32:41 +0300

    Implement Service layer

---
+++ src/main/java/com/ico/ltd/spring5mvcrest/services/CategoryService.java
@@ -0,0 +1,12 @@
+package com.ico.ltd.spring5mvcrest.services;
+
+import com.ico.ltd.spring5mvcrest.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
